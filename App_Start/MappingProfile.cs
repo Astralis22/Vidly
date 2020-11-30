ca425691ba402d4d3699f40567037ab5ec992213
Andrea Bombara <andrea.bombara@outlook.it>
2020-11-30 12:11:12 +0100

    Section 6 exercise done

--- App_Start/MappingProfile.cs
+++ App_Start/MappingProfile.cs
@@ -14,10 +14,12 @@ namespace Vidly.App_Start
         {
             // Domain to Dto
             Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
 
             // Dto to Domain
             //Mapper.CreateMap<CustomerDto, Customer>();
-            Mapper.CreateMap<CustomerDto,Customer>().ForMember(c => c.Id, opt=>opt.Ignore());
+            Mapper.CreateMap<CustomerDto,Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
---
+++ Dtos/MovieDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter movie's title.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required(ErrorMessage = "The field Number in Stock must be between 1 and 20")]
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
