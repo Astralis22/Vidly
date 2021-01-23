61d5ab4e22f0f307b996b577bbd7c0b008bb112e
Andrea Bombara <andrea.bombara@outlook.it>
2021-01-23 18:36:16 +0100

    Added basic return rented movie

--- Controllers/RentalsController.cs
+++ Controllers/RentalsController.cs
@@ -34,5 +34,20 @@ namespace Vidly.Controllers
 
             return View(rentals);
         }
+
+        public ActionResult EndRental(int id)
+        {
+            var rental = _context.Rentals.SingleOrDefault(r => r.Id == id);
+
+            if (rental == null)
+                return HttpNotFound();
+
+            rental.DateReturned = DateTime.Now;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("List","Rentals");
+        }
+
     }
 }
