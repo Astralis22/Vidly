7cdb96ffc9bd49431dfc5715373c5149ab77010d
Andrea Bombara <andrea.bombara@outlook.it>
2021-01-30 18:15:22 +0100

    Showing assigned roles working, using RoleManager to add/delete roles

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,11 +2,13 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -31,8 +33,30 @@ namespace Vidly.Controllers
 
         public ActionResult Users()
         {
-            var users = _context.Users.ToList();
-            return View(users);
+            var users = _context.Users.Include( u => u.Roles).ToList();
+
+            var roleStore = new RoleStore<IdentityRole>(_context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            List<UserViewModel> userViewModels = new List<UserViewModel>();
+
+            foreach ( var user in users)
+            {
+                var userViewModel = new UserViewModel
+                {
+                    User = user,
+                    Roles = new List<string>()
+                };
+
+                var roleIdList = user.Roles.Select(r => r.RoleId);
+                foreach ( var roleId in roleIdList)
+                {
+                    var roleName = roleManager.FindById(roleId).Name;
+                    userViewModel.Roles.Add(roleName);
+                }
+                userViewModels.Add(userViewModel);
+            }
+            return View(userViewModels);
         }
 
         public ActionResult Roles()
@@ -50,23 +74,48 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult SaveRole(IdentityRole role)
         {
-            _context.Roles.Add(role);
-            _context.SaveChanges();
+            //_context.Roles.Add(role);
+            //_context.SaveChanges();
+
+            var roleStore = new RoleStore<IdentityRole>(_context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            roleManager.Create(role);
+
 
             return RedirectToAction("Roles", "Admin");
         }
 
         public ActionResult DeleteRole(string id)
         {
-            var roleInDb = _context.Roles.SingleOrDefault(r => r.Id == id);
+            //var roleInDb = _context.Roles.SingleOrDefault(r => r.Id == id);
 
-            if(roleInDb == null)
-                return HttpNotFound();
+            //if(roleInDb == null)
+            //    return HttpNotFound();
 
-            _context.Roles.Remove(roleInDb);
-            _context.SaveChanges();
+            var roleStore = new RoleStore<IdentityRole>(_context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            var roleIndDb = roleManager.FindById(id);
+            roleManager.Delete(roleIndDb);
+
+            //_context.Roles.Remove(roleInDb);
+            //_context.SaveChanges();
 
             return RedirectToAction("Roles", "Admin");
+        }
+
+        public ActionResult AssignRole(string userId, string roleId)
+        {
+
+            var roleStore = new RoleStore<IdentityRole>(_context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+            var roleInDb = roleManager.FindById(roleId);
+
+            var userStore = new UserStore<IdentityUser>(_context);
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var userInDb = userManager.FindById(userId);
+            userManager.AddToRole(userInDb.Id, roleInDb.Name);     
+
+            return View();
         }
     }
 }
---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class UserViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
